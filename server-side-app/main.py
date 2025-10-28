9e178a35cc39ec09d436da962d3a500b68169d42
arif-youknow <inbox.arifkhan@gmail.com>
2025-10-28 15:33:05 +0600

    server created

--- server-side-app/main.py
+++ server-side-app/main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from fastapi import FastAPI
+from app.core.database import Base, engine
+from app.models import lost_items
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Lost and Found API",
+    description="A simple API for managing lost and found items.",
+    version="1.0.0",
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Lost and Found API"}
+
